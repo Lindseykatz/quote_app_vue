4005c019f7339a18be7cba37fed74f15cbdb1c81
Lindsey Katz <katz.lindsey@gmail.com>
2019-07-23 19:22:57 -0500

    Add pagination - still need to apply 15 quotes per page

--- src/app.js
+++ src/app.js
@@ -3,16 +3,15 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 
-axios.defaults.baseURL =
-  process.env.NODE_ENV === "development"
-    ? "https://gist.githubusercontent.com/benchprep/dffc3bffa9704626aa8832a3b4de5b27/raw/quotes.json"
-    : "/";
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
 const url = "https://gist.githubusercontent.com/benchprep/dffc3bffa9704626aa8832a3b4de5b27/raw/quotes.json";
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount("#app");
+  el: "#app",
+  data: {
+    placeholder: "BenchPrep JavaScript Exercise"
+  }
+});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--     <button id="btn" class="" v-on:click="getQuotes">Get Quotes</button> -->
+    <div class="quotes" v-for="quote in quotes">
+      <ul>
+        "{{
+          quote.quote
+        }}" -
+        {{
+          quote.source
+        }},
+        {{
+          quote.context
+        }}
+      </ul>
+    </div>
+    <div class="pagination">
+      <a href="#">&laquo;</a>
+      <a class="active" href="#">1</a>
+      <a href="#">2</a>
+      <a href="#">3</a>
+      <a href="#">4</a>
+      <a href="#">5</a>
+      <a href="#">6</a>
+      <a href="#">&raquo;</a>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import axios from "axios";
+import "../../public/quotes.css";
+
+const url = "https://gist.githubusercontent.com/benchprep/dffc3bffa9704626aa8832a3b4de5b27/raw/quotes.json";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: "app",
+  data: function() {
+    return {
+      quotes: []
+    };
+  },
+  created: function() {
+    axios.get(url).then(response => {
+      this.quotes = response.data;
+    });
+    console.log(this.quotes);
   }
-}
+};
 </script>
